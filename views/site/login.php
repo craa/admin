090ae088f2dbcb40d4c70e5d8ecc002f341fc09d
craa <412280471@qq.com>
2015-10-12 17:39:07 +0800

    修改登录页

--- views/site/login.php
+++ views/site/login.php
@@ -1,39 +1,33 @@
 <div class="row">
     <div class="col-md-12 center login-header">
-        <h2>Welcome to Charisma</h2>
+        <h2>欢迎使用 Admin</h2>
     </div>
     <!--/span-->
 </div><!--/row-->
 
 <div class="row">
     <div class="well col-md-5 center login-box">
-        <div class="alert alert-info">
-            Please login with your Username and Password.
+        <div id="topTip" class="alert alert-info">
+            <div id="tips">请输入用户名密码</div>
         </div>
-        <form class="form-horizontal" action="index.html" method="post">
-            <fieldset>
-                <div class="input-group input-group-lg">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user red"></i></span>
-                    <input type="text" class="form-control" placeholder="Username">
-                </div>
-                <div class="clearfix"></div><br>
 
-                <div class="input-group input-group-lg">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock red"></i></span>
-                    <input type="password" class="form-control" placeholder="Password">
-                </div>
-                <div class="clearfix"></div>
+        <?php $form = \yii\bootstrap\ActiveForm::begin(['options'=>['class'=>'form-horizontal']]); ?>
+        <filedset>
+            <?= $form->field($model, 'username', ['options'=>['class'=>'input-group input-group-lg'], 'template'=>"{label}\n{input}", 'inputOptions'=>['placeholder'=>'用户名']])
+                ->label('<i class="glyphicon glyphicon-user red"></i>', ['class'=>'input-group-addon']); ?>
 
-                <div class="input-prepend">
-                    <label class="remember" for="remember"><input type="checkbox" id="remember"> Remember me</label>
-                </div>
-                <div class="clearfix"></div>
+            <div class="clearfix"></div><br>
 
-                <p class="center col-md-5">
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </p>
-            </fieldset>
-        </form>
+            <?= $form->field($model, 'password', ['options'=>['class'=>'input-group input-group-lg'], 'template'=>"{label}\n{input}", 'inputOptions'=>['placeholder'=>'密码', 'type'=>'password']])
+                ->label('<i class="glyphicon glyphicon-lock red"></i>', ['class'=>'input-group-addon']); ?>
+
+            <?= $form->field($model, 'rememberMe', ['options'=>['class'=>'input-prepend']])->checkbox(['value'=>0])->label('记住我', ['class'=>'remember'])?>
+
+            <p class="center col-md-5">
+                <button type="submit" class="btn btn-primary">登录</button>
+            </p>
+        </filedset>
+        <?php \yii\bootstrap\ActiveForm::end(); ?>
     </div>
     <!--/span-->
 </div><!--/row-->
